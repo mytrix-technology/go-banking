2510950a5f05f51b70bb7f8b1bdf3297a37b3acf
Yudhistiro Tri Aronggo <yudhistiro.aronggo@uangteman.com>
2020-05-20 23:18:01 +0700

    first commit

---
+++ helpers/helper.go
@@ -0,0 +1,17 @@
+package helper
+
+import "golang.org/x/crypto/bcrypt"
+
+func HandleErr(err error)  {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func HashAndSalt(pass []byte) string {
+	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+
+	HandleErr(err)
+
+	return string(hashed)
+}
